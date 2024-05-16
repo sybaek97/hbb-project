5ffb10ac82b0ff80c36fa9a0d14699ced2c3dc3c
baeksy97 <baeksy1018@gmail.com>
2024-05-16 23:13:07 +0900

    [재고조회] 로케이션 재고이동 ui 개발

--- app/src/main/java/com/bhh/app/view/inventoryCheck/LocationStockTransferFragment.kt
+++ app/src/main/java/com/bhh/app/view/inventoryCheck/LocationStockTransferFragment.kt
@@ -4,11 +4,14 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ArrayAdapter
+import android.widget.Spinner
+import androidx.core.view.isVisible
 import androidx.databinding.DataBindingUtil
 import com.bhh.app.R
 import com.bhh.app.application.BaseFragment
-import com.bhh.app.databinding.FragmentBoxContentsBinding
 import com.bhh.app.databinding.FragmentLocationStockTransferBinding
+
 
 class LocationStockTransferFragment : BaseFragment() {
     override var isBackAvailable: Boolean=false
@@ -26,5 +29,21 @@ class LocationStockTransferFragment : BaseFragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         binding.lifecycleOwner = this
+
+        binding.checkStage.setOnCheckedChangeListener { _, isChecked ->
+            binding.checkGeneralMove.isChecked = !isChecked
+            binding.stageContain.isVisible = isChecked
+        }
+        binding.checkGeneralMove.setOnCheckedChangeListener { _, isChecked ->
+            binding.checkStage.isChecked = !isChecked
+            binding.stageContain.isVisible = !isChecked
+        }
+        val simpleSpinner: Spinner = binding.selectSpinner
+        val adapter = ArrayAdapter.createFromResource(
+            requireContext(),
+            R.array.spinner_items, android.R.layout.simple_spinner_item
+        )
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
+        simpleSpinner.adapter = adapter
     }
 }
